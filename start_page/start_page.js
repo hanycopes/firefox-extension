82473b4a753b1431c95bd2c791f8435225bfa563
hanycopes <hanisarduk93@gmail.com>
2021-03-21 23:41:05 +0200

    "update js file"

--- start_page/start_page.js
+++ start_page/start_page.js
@@ -1,14 +1,40 @@
-// This code is supoosed to print the bookmarks to the console
+function makeIndent(indentLength) {
+  return ".".repeat(indentLength);
+}
 
-function onFulfilled(bookmarks) {
-    console.log(bookmarks);
+function logItems(bookmarkItem, indent) {
+  if (bookmarkItem.url) {
+    var urls = bookmarkItem.url
+    var limit = urls.length
+    for (var i = 1; i < limit ; i +=1){
+      var ids = "bookmark-" + i
+      updateHTML(ids,urls)
   }
-  
-  function onRejected(error) {
-    console.log(`An error: ${error}`);
-  }
-  
-  var gettingBookmarks = browser.bookmarks.get("bookmarkAAAA");
-  gettingBookmarks.then(onFulfilled, onRejected);
 
-  // Then we will have a function 
+  } else {
+    console.log(makeIndent(indent) + "Bookmark");
+    indent++;
+  }
+  if (bookmarkItem.children) {
+    for (child of bookmarkItem.children) {
+      logItems(child, indent);
+    }
+  }
+  indent--;
+}
+
+function logTree(bookmarkItems) {
+  logItems(bookmarkItems[0], 0);
+}
+
+function onRejected(error) {
+  console.log(`An error: ${error}`);
+}
+function updateHTML(elmId, value) {
+  var elem = document.getElementById(elmId);
+  if(typeof elem !== 'undefined' && elem !== null) {
+    elem.innerHTML = value;
+  }
+}
+var gettingTree = browser.bookmarks.getTree();
+gettingTree.then(logTree, onRejected);
